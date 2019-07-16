39021ad328b74b76d4de557bd1b9921f3e9eed62
y_lin35@fanshaweonline.ca <46694893+TomLin19@users.noreply.github.com>
2019-07-16 13:18:48 -0400

    week10

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-(()=>{
+(() =>{
 	console.log("fired!");
-})()
+
+	let mobileNav = document.querySelector(".nav")
+	    navPanel = document.querySelector('#burgerCon');
+
+	function toggleNa() {
+		console.log('should show nav dropdown');
+		navPanel.classList.toggle('show-mobile-nav');
+
+	}
+
+	mobileNav.addEventListener('click',toggleNa);
+})();
